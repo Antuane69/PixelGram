f16f148f73dcf66fcc506f8680e2be059e168610
Antuane69 <antuanealex49@gmail.com>
2024-09-15 22:39:35 -0600

    Se empezo a diseñar el dashboard con un text area para crear publicaciones, un apartado para ver las publicaciones tuyas y de todos los usuarios y un apartado para ver tu perfil, seria como estadistico y de diseño, para cambiar cosas desde ahi, probablemente en un futuro un semi-chat en tiempo real con seguidores

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,17 +1,35 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
+import Post from './Posts/Post';
 
 export default function Dashboard() {
     return (
         <AuthenticatedLayout
-            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>}
+            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Publicaciones</h2>}
         >
-            <Head title="Dashboard" />
+            <Head title="PixelGram" />
 
             <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">You're logged in!</div>
+                <div className="flex">
+                    <div className="ml-5 mr-2 sm:px-6 lg:px-8 space-y-6 w-2/3 overflow-hidden sm:rounded-lg content-center justify-center">
+                        <div className="py-6 text-center justify-center rounded-lg bg-white shadow-md">
+                            <p className="mb-6 font-bold text-lg">Haz una publicación:</p>
+                            <div className="flex">
+                                <textarea name="Post" id="post-id" className="w-4/5 border-2 rounded-lg h-24 ml-7"></textarea>
+                                <button className="bg-green-800 text-white font-bold ml-8 mt-14 hover:bg-green-700 px-3 rounded-md">Publicar</button>
+                            </div>
+                        </div>
+                        <div className="overflow-hidden rounded-lg p-6 bg-white shadow-sm">
+                            <span className="p-6 text-gray-900 mt-4">You're logged in!</span>
+                        </div>
+                    </div>
+                    <div className="w-1/3 mr-9 bg-white shadow-sm sm:rounded-lg content-center justify-center flex">
+                        <div className=" mt-6">
+                            <p>User Profile:</p>
+                        </div>
+                        <div className=" mt-6">
+                            <span className="p-6 text-gray-900 mt-4">You're logged in!</span>
+                        </div>
                     </div>
                 </div>
             </div>
---
+++ resources/js/Pages/Posts/Post.jsx
@@ -0,0 +1,23 @@
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+import { Head } from '@inertiajs/react';
+
+export default function Post() {
+    return (
+        <Post
+            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Posts</h2>}
+        >
+            <Head title="Posts" />
+
+            <div className="py-12">
+                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg space-y-2">
+                        <div>
+                            <textarea name="Post" id="post-id" className="w-5/6 h-auto"></textarea>
+                        </div>
+                        <div className="p-6 text-gray-900 mt-4">You're logged in!</div>
+                    </div>
+                </div>
+            </div>
+        </Post>
+    );
+}
